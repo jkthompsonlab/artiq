3805233e971e6e38e0e918402f0d2aa953bdb9a1
Ada <vesc6053@colorado.edu>
2022-02-11 14:50:21 -0700

    kasli_cw: add more modules

--- artiq/gateware/targets/kasli_cw.py
+++ artiq/gateware/targets/kasli_cw.py
@@ -42,7 +42,12 @@ class Master(MasterBase):
         eem.Urukul.add_std(self, 0, 1, ttl_serdes_7series.Output_8X, ttl_simple.ClockGen)
         eem.Urukul.add_std(self, 2, 3, ttl_serdes_7series.Output_8X, ttl_simple.ClockGen)
         eem.DIO.add_std(self, 4, ttl_serdes_7series.InOut_8X, ttl_serdes_7series.Output_8X, edge_counter_cls=edge_counter.SimpleEdgeCounter)
-        eem.Mirny.add_std(self,5, ttl_serdes_7series.Output_8X)
+        eem.Urukul.add_std(self, 5, 6, ttl_serdes_7series.Output_8X, ttl_simple.ClockGen)
+        eem.Sampler.add_std(self, 7, 8, ttl_serdes_7series.Output_8X)
+        eem.Zotino.add_std(self, 9, ttl_serdes_7series.Output_8X)
+        eem.DIO.add_std(self, 10, ttl_serdes_7series.Output_8X)
+        eem.Mirny.add_std(self,11, ttl_serdes_7series.Output_8X)
+        
 
         self.config["HAS_RTIO_LOG"] = None
         self.config["RTIO_LOG_CHANNEL"] = len(self.rtio_channels)
